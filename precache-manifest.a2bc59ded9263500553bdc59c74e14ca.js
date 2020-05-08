6d454bc939935871a433d4f9a0e251c09bd39e07
Manjunathhebbar K(Associate) <M9012754@mindtree.com>
2020-05-08 14:23:51 +0530

    Updates

---
+++ precache-manifest.a2bc59ded9263500553bdc59c74e14ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf1840ea72575e169afae2b60d9723a",
+    "url": "/react-authentication/index.html"
+  },
+  {
+    "revision": "bbd1fc658355eb7a9bcc",
+    "url": "/react-authentication/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "1d6d31fbe96766e5cc3f",
+    "url": "/react-authentication/static/css/main.2300c55e.chunk.css"
+  },
+  {
+    "revision": "bbd1fc658355eb7a9bcc",
+    "url": "/react-authentication/static/js/2.efdf08f0.chunk.js"
+  },
+  {
+    "revision": "1d6d31fbe96766e5cc3f",
+    "url": "/react-authentication/static/js/main.b6a2ebc9.chunk.js"
+  },
+  {
+    "revision": "5adb89c95c2af88ecb6b",
+    "url": "/react-authentication/static/js/runtime-main.92092c4a.js"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/react-authentication/static/media/bars-solid.b3f49afd.svg"
+  },
+  {
+    "revision": "144ff03084284c8876293946a229fd8e",
+    "url": "/react-authentication/static/media/sign-out-alt-solid.144ff030.svg"
+  }
+]);
